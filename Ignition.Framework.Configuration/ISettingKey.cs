dacf9e117b82975b1fa5e18f21a3bd80407708c0
Jon Upchurch <jon.upchurch@perficient.com>
2018-07-14 17:45:55 -0400

    Added configuration classes

---
+++ Ignition.Framework.Configuration/ISettingKey.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ignition.Framework.Configuration
+{
+    public interface ISettingKey
+    {
+        string Key { get; }
+    }
+}
---
+++ Ignition.Framework.Configuration/ISettingsProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ignition.Framework.Configuration
+{
+    public interface ISettingsProvider
+    {
+        object GetSetting(ISettingKey settingKey);
+        TItem GetSetting<TItem>(ISettingKey settingKey) where TItem : ISettingsItem;
+        TItem GetSetting<TItem>(string settingKey) where TItem : ISettingsItem;
+        IEnumerable<TItem> GetAllSettingsOfType<TItem>() where TItem : ISettingsItem;
+    }
+}
